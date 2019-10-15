92e76d01c1b6a243fc180c5d384e2e8e09f85729
Dima Krasner <dima@dimakrasner.com>
2019-10-15 22:15:53 +0300

    ci: cleanup

--- ci.sh
+++ ci.sh
@@ -47,9 +47,6 @@ IPV6_ONLY_DOMAINS="
 meson --prefix=/usr --buildtype=release -Dstrip=true -Dresolvers=https://9.9.9.9/dns-query+random,https://dns.google/dns-query+random,https://1.1.1.1/dns-query+random build
 ninja -C build install
 
-meson configure build
-ninja -C build
-
 CC=clang meson --prefix=/usr -Db_sanitize=address build-asan
 ninja -C build-asan nss-tlsd
 
